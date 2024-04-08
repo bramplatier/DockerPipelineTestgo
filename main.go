626f0c23780ce7c8e0e1222bb2dd1b7211bf181e
bramplatier <145543039+bramplatier@users.noreply.github.com>
2024-04-08 11:28:13 +0200

    Update main.go

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 	e.Use(middleware.Recover())
 
 	e.GET("/", func(c echo.Context) error {
-		return c.HTML(http.StatusOK, "Timo is gay")
+		return c.HTML(http.StatusOK, "Timo is gay 123")
 	})
 
 	e.GET("/health", func(c echo.Context) error {
